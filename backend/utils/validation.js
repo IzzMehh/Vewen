0587a24180cc95ad3582084db5f0a4e9b88223db
Gaurav <gauravbishtyt@gmail.com>
2024-10-01 19:44:32 +0530

    validation fixes

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -8,11 +8,11 @@ const authValidation = joi.object({
     }),
 
     username: joi.string()
-    .alphanum()
+    .pattern(new RegExp('^[a-zA-Z0-9_]+$'))
     .min(3)
     .max(15)
     .messages({
-      'string.alphanum': 'Username can only contain letters and numbers.',
+      'string.pattern.base': 'Username can only contain letters, numbers, and underscores.',
       'string.min': 'Username should be at least 3 characters long.',
       'string.max': 'Username should not exceed 15 characters.',
     }),
@@ -20,11 +20,9 @@ const authValidation = joi.object({
     password: joi.string()
     .min(8)
     .max(30)
-    .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])'))
     .messages({
       'string.min': 'Password must be at least 8 characters long.',
       'string.max': 'Password should not exceed 30 characters.',
-      'string.pattern.base': 'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.',
     })
 })
 
